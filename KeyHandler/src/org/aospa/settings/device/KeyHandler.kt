602e518f6a786c4e67dfda4a27aa48c30f1b385e
Arian <arian.kulmer@web.de>
2025-09-12 18:44:15 +0200

    KeyHandler: Make allowed device names configurable
    Change-Id: Iabd62357f97698c440b940f92dc61c7ad25e1084

--- KeyHandler/src/org/aospa/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/aospa/settings/device/KeyHandler.kt
@@ -74,7 +74,7 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
 
         val deviceName = event.device.name
 
-        if (deviceName != "oplus,hall_tri_state_key" && deviceName != "oplus,tri-state-key") {
+        if (deviceName == null || !packageContext.resources.getStringArray(R.array.config_alertSliderDeviceNames).contains(deviceName)) {
             return event
         }
 
