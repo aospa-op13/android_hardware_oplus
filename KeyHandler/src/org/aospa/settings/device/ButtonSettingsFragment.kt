0c07464d0efc82349aed719420f5368b60bac0fb
Bruno Martins <bgcngm@gmail.com>
2025-11-23 22:35:05 +0000

    KeyHandler: Modernize ButtonSettings
    Fully convert to AndroidX fragments and stop overriding
    OnOptionsItemSelected(IMenuItem).
    
    Change-Id: I56261b2bfd47bd5dd75ca52cb463e76309a14dce

--- KeyHandler/src/org/aospa/settings/device/ButtonSettingsFragment.kt
+++ KeyHandler/src/org/aospa/settings/device/ButtonSettingsFragment.kt
@@ -1,45 +1,16 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package co.aospa.settings.device
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragment
+import androidx.preference.PreferenceFragmentCompat
 
-class ButtonSettingsFragment : PreferenceFragment() {
+class ButtonSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.button_panel)
-        activity.actionBar!!.setDisplayHomeAsUpEnabled(true)
-
-        val positionKeyMap = mapOf(
-            KeyHandler.POSITION_TOP to KeyHandler.ALERT_SLIDER_TOP_KEY,
-            KeyHandler.POSITION_MIDDLE to KeyHandler.ALERT_SLIDER_MIDDLE_KEY,
-            KeyHandler.POSITION_BOTTOM to KeyHandler.ALERT_SLIDER_BOTTOM_KEY,
-        )
-
-        positionKeyMap.forEach { (position, key) ->
-            if (!KeyHandler.isPositionSupported(context, position)) {
-                val unusupportedPref = findPreference<Preference>(key)
-                unusupportedPref?.parent?.removePreference(unusupportedPref)
-            }
-        }
-    }
-
-    override fun addPreferencesFromResource(preferencesResId: Int) {
-        super.addPreferencesFromResource(preferencesResId)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.home -> {
-                activity.finish()
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+        setPreferencesFromResource(R.xml.button_panel, rootKey)
     }
 }
